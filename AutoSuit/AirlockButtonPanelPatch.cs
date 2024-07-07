1fa4594e9c421eeab2c23ff965ee45db704094ba
18107 <18107@users.noreply.github.com>
2024-07-07 09:57:14 +0930

    Bug fix

--- AutoSuit/AirlockButtonPanelPatch.cs
+++ AutoSuit/AirlockButtonPanelPatch.cs
@@ -1,30 +1,37 @@
 ﻿using CG.Client.Ship;
+using CG.Game;
 using CG.Game.Player;
 using CG.Ship.Hull;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace AutoSuit
 {
     [HarmonyPatch(typeof(AirlockButtonPanel))]
     internal class AirlockButtonPanelPatch
     {
+        private static readonly FieldInfo flyAbilityField = AccessTools.Field(typeof(JetpackItem), "_flyAbility");
+
         [HarmonyPrefix]
         [HarmonyPatch("OnPressurizeButtonPressed")]
         static void OnPressurizeButtonPressed(AirlockButtonPanel __instance, AirlockPressurizationType type, Airlock ____airlock)
         {
             if (__instance.name == "AirlockButtonPanel (1)" && !____airlock.IsPressurizing && !____airlock.airlockDoors.Any(door => door.IsOpen))
             {
+                ItemEquipper equipper = ClientGame.Current.PlayerShip.gameObject.GetComponentsInChildren<ItemEquipper>().FirstOrDefault(item => item.name == "JetpackRack");
+                if (equipper == null) return;
                 JetpackItem jetpack = LocalPlayer.Instance.gameObject.GetComponentInChildren<JetpackItem>();
-                if (type == AirlockPressurizationType.Pressurize)
+                FlyJetpack flyAbility = (FlyJetpack)flyAbilityField.GetValue(jetpack);
+                if (type == AirlockPressurizationType.Pressurize && flyAbility.Enabled)
                 {
-                    DelayDo(jetpack.Unequip, 2000); //TODO replace with VoidManager.Utilities.Tools.DelayDo
+                    DelayDo(() => { if (flyAbility.Enabled) equipper.ToggleEquippedItem(); }, 2000); //TODO replace with VoidManager.Utilities.Tools.DelayDo
                 }
                 else
                 {
-                    jetpack.Equip(true);
+                    equipper.ToggleEquippedItem();
                 }
             }
         }
